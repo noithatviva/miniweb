439689760974d5704c861e044c94dff9bea1938e
Nội thất Viva <120771445+noithatviva@users.noreply.github.com>
2022-12-17 16:20:24 +0700

    Update custom_botplus.js

--- custom_botplus.js
+++ custom_botplus.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
    
 });
 document.addEventListener("DOMContentLoaded", function (event) {
-   
   sleep(2500).then(() => { 
+    $(".product__detail").html($(".product__detail").html().replace(/&nbsp;/g,''));
+  });
+  sleep(3000).then(() => { 
     var btn_cart = document.querySelector('#product .product__cart');
     if(btn_cart != null)
     {
       const text = document.createTextNode('Đặt hàng');
       btn_cart.appendChild(text);
     }
-    $(".product__detail").html($(".product__detail").html().replace(/&nbsp;/g,''));
-    
   });
 });
